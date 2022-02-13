29a70f3139ada8db15e5dc87620e9d78c14655b6
sorappy <murakumo40317@gmail.com>
2022-02-13 21:42:25 +0900

    ログインページ追加

---
+++ src/admin.php
@@ -0,0 +1,102 @@
+<?php
+
+//管理ページのログインパスワード
+define( 'PASSWORD', 'adminPassword');
+
+// データベースの接続情報
+define( 'DB_HOST', 'mysql');
+define( 'DB_USER', 'root');
+define( 'DB_PASS', 'root');
+define( 'DB_NAME', 'board');
+
+// タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+// 変数の初期化
+$current_date = null;
+$message = array();
+$message_array = array();
+$success_message = null;
+$error_message = array();
+$pdo = null;
+$stmt = null;
+$res = null;
+$option = null;
+
+session_start();
+
+// データベースに接続
+try {
+    $option = array(
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+	);
+    $pdo = new PDO('mysql:charset=UTF8;dbname='.DB_NAME.';host='.DB_HOST , DB_USER, DB_PASS, $option);
+
+
+} catch(PDOException $e) {
+
+    // 接続エラーのときエラー内容を取得する
+    $error_message[] = $e->getMessage();
+}
+
+if( !empty($_POST['btn_submit']) ) {
+    if( !empty($_POST['admin_password']) && $_POST['admin_password'] === PASSWORD ) {
+        $_SESSION['admin_login'] = true;
+    } else {
+        $error_message[] = 'ログイン失敗しました。';
+    }
+}
+
+if( empty($error_message) ) {
+
+	// メッセージのデータを取得する
+	$sql = "SELECT view_name,message,post_date FROM message ORDER BY post_date DESC";
+	$message_array = $pdo->query($sql);
+}
+
+// データベースの接続を閉じる
+$pdo = null;
+//echo phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>ひと言掲示板 管理ページ</title>
+<link rel="stylesheet" href="index.css">
+</head>
+<body>
+<h1>ひと言掲示板 管理ページ</h1>
+<?php if( !empty($error_message) ): ?>
+	<ul class="error_message">
+		<?php foreach( $error_message as $value ): ?>
+			<li>・<?php echo $value; ?></li>
+		<?php endforeach; ?>
+	</ul>
+<?php endif; ?>
+<section>
+<?php if( !empty($_SESSION['admin_login']) && $_SESSION['admin_login'] === true ): ?>
+<?php if( !empty($message_array) ): ?>
+<?php foreach( $message_array as $value ): ?>
+<article>
+    <div class="info">
+        <h2><?php echo htmlspecialchars( $value['view_name'], ENT_QUOTES, 'UTF-8'); ?></h2>
+        <time><?php echo date('Y年m月d日 H:i', strtotime($value['post_date'])); ?></time>
+    </div>
+    <p><?php echo nl2br( htmlspecialchars( $value['message'], ENT_QUOTES, 'UTF-8') ); ?></p>
+</article>
+<?php endforeach; ?>
+<?php endif; ?>
+<?php else: ?>
+<form method="post">
+    <div>
+        <label for="admin_password">ログインパスワード</label>
+        <input id="admin_password" type="password" name="admin_password" value="">
+    </div>
+    <input type="submit" name="btn_submit" value="ログイン">
+</form>
+<?php endif; ?>
+</section>
+</body>
+</html>
