d76d812ad03156d3951485561418beaa78f9efaa
sorappy <murakumo40317@gmail.com>
2022-04-18 00:40:02 +0900

    投稿データをデータベースに登録する->コード調整

--- src/index.php
+++ src/index.php
@@ -44,20 +44,17 @@ if( !empty($_POST['btn_submit']) ) {
     $message = preg_replace( '/\A[\p{C}\p{Z}]++|[\p{C}\p{Z}]++\z/u', '', $_POST['message']);
 
  	// 表示名の入力チェック
-     if( empty($_POST['view_name']) ) {
+     if( empty($view_name) ) {
 		$error_message[] = '表示名を入力してください。';
 	} else {
-		$clean['view_name'] = htmlspecialchars( $_POST['view_name'], ENT_QUOTES, 'UTF-8');
-        $clean['view_name'] = preg_replace( '/\\r\\n|\\n|\\r/', '', $clean['view_name']);
         $_SESSION['view_name'] = $view_name;
 	}
 
 	// メッセージの入力チェック
-	if( empty($_POST['message']) ) {
+	if( empty($message) ) {
 		$error_message[] = 'ひと言メッセージを入力してください。';
 	} else {
-		$clean['message'] = htmlspecialchars( $_POST['message'], ENT_QUOTES, 'UTF-8');
-        $clean['message'] = preg_replace( '/\\r\\n|\\n|\\r/', '<br>', $clean['message']);
+
 	}
 
     if( empty($error_message) ) {
@@ -74,8 +71,8 @@ if( !empty($_POST['btn_submit']) ) {
             $stmt = $pdo->prepare("INSERT INTO message (view_name, message, post_date) VALUES ( :view_name, :message, :current_date)");
 
             // 値をセット
-            $stmt->bindParam( ':view_name', $clean['view_name'], PDO::PARAM_STR);
-            $stmt->bindParam( ':message', $clean['message'], PDO::PARAM_STR);
+            $stmt->bindParam( ':view_name', $view_name, PDO::PARAM_STR);
+            $stmt->bindParam( ':message', $message, PDO::PARAM_STR);
             $stmt->bindParam( ':current_date', $current_date, PDO::PARAM_STR);
 
             // SQLクエリの実行
