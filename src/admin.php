f8b131d3700e12850bcd2f7328362ed34d763679
sorappy <murakumo40317@gmail.com>
2022-04-17 22:30:52 +0900

    投稿データをダウンロードする件数を指定する

--- src/admin.php
+++ src/admin.php
@@ -82,6 +82,11 @@ $pdo = null;
 <section>
 <?php if( !empty($_SESSION['admin_login']) && $_SESSION['admin_login'] === true ): ?>
 <form method="get" action="./download.php">
+    <select name="limit">
+        <option value="">全て</option>
+        <option value="10">10件</option>
+        <option value="30">30件</option>
+    </select>
     <input type="submit" name="btn_download" value="ダウンロード">
 </form>
 <?php if( !empty($message_array) ): ?>
--- src/download.php
+++ src/download.php
@@ -22,8 +22,19 @@ $pdo = null;
 $stmt = null;
 $res = null;
 $option = null;
+$limit = null;
+
 
 session_start();
+
+//取得件数
+if(!empty($_GET['limit'])){
+    if($_GET['limit'] === "10"){
+        $limit = 10;
+    } elseif ( $_GET['limit'] === "30" ){
+        $limit = 30;
+    }
+}
 
 if( !empty($_SESSION['admin_login']) && $_SESSION['admin_login'] === true ) {
     //データベースに接続
@@ -35,9 +46,23 @@ if( !empty($_SESSION['admin_login']) && $_SESSION['admin_login'] === true ) {
         $pdo = new PDO('mysql:charset=UTF8;dbname='.DB_NAME.';host='.DB_HOST , DB_USER, DB_PASS, $option);
 
         //メッセージのデータを取得する
-        $sql = "SELECT * FROM message ORDER BY post_date ASC";
-        $message_array = $pdo->query($sql);
+//        $sql = "SELECT * FROM message ORDER BY post_date ASC";
+//        $message_array = $pdo->query($sql);
+        if( !empty($limit) ) {
+            //SQL作成
+            $stmt = $pdo->prepare("SELECT * FROM message ORDER BY post_date ASC LIMIT :limit");
+            //値をセット
+            $stmt->bindValue( ':limit', $limit, PDO::PARAM_INT );
+        } else {
+            $stmt = $pdo->prepare("SELECT * FROM message ORDER BY post_date ASC");
+        }
+
+        //SQLクエリの実行
+        $stmt->execute();
+        $message_array = $stmt->fetchAll();
+
         // データベースの接続を閉じる
+        $stmt = null;
         $pdo = null;
     } catch(PDOException $e) {
         //管理者ページへリダイレクト
